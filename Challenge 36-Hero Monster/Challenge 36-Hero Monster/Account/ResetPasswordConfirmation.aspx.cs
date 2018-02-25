8a7ba1d139fff414c7e36d5bed229d8214b186f1
ThunderDart <caalexmartin@gmail.com>
2018-02-25 13:13:55 -0800

    remove challenge 36

--- Challenge 36-Hero Monster/Challenge 36-Hero Monster/Account/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace Challenge_36_Hero_Monster.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
--- Challenge 36-Hero Monster/Challenge 36-Hero Monster/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Challenge_36_Hero_Monster.Global" Language="C#" %>
